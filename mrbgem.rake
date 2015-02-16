1172e857656ff20055e4ad65373dfa0b08ef315c
masahino <masahino@gmail.com>
2015-02-16 23:04:27 +0900

    first commit

---
+++ mrbgem.rake
@@ -0,0 +1,4 @@
+MRuby::Gem::Specification.new('mruby-scintilla-base') do |spec|
+  spec.license = 'MIT'
+  spec.authors = 'masahino'
+end
---
+++ mrblib/scintilla-base.rb
@@ -0,0 +1,17 @@
+module Scintilla
+  class ScintillaBase
+    def method_missing(method_name, *args)
+      message_id = "SCI_"+method_name.to_s.gsub("_", "").upcase
+      send_message(eval(message_id), *args)
+    end
+    
+    def set_text(text)
+      self.send_message(SCI_SETTEXT, 0, text)
+    end
+    
+    def get_focus()
+      ret = send_message(SCI_GETFOCUS)
+      return ( ret != 0 )? true : false
+    end
+  end
+end
