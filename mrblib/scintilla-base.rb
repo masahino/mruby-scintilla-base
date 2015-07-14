128b2a308c96aea866ca5125eb8bfe077eb39c11
masahino <masahino@gmail.com>
2015-07-14 20:39:33 +0900

    change method name

--- mrblib/scintilla-base.rb
+++ mrblib/scintilla-base.rb
@@ -1,20 +1,25 @@
 module Scintilla
   class ScintillaBase
     def method_missing(method_name, *args)
-      message_id = "SCI_"+method_name.to_s.gsub("_", "").upcase
-      send_message(eval(message_id), *args)
+      if method_name.to_s[0..3].upcase == "SCI_"
+        message_id = "SCI_"+method_name.to_s[4..-1].gsub("_", "").upcase
+        send_message(eval(message_id), *args)
+      else
+        $stderr.puts "method missing #{method_name}"
+        super
+      end
     end
     
-    def set_text(text)
+    def sci_set_text(text)
       self.send_message(SCI_SETTEXT, 0, text)
     end
     
-    def get_focus()
+    def sci_get_focus()
       ret = send_message(SCI_GETFOCUS)
       return ( ret != 0 )? true : false
     end
     
-    def autoc_select(select)
+    def sci_autoc_select(select)
       self.send_message(SCI_AUTOCSELECT, 0, select)
     end
   end
