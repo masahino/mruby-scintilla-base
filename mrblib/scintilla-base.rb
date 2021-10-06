9ebc61d1734bdec02032f5fac4be93cd2cc67276
masahino <masahino@gmail.com>
2021-10-07 07:22:00 +0900

    introduce respond_to_missing?

--- mrblib/scintilla-base.rb
+++ mrblib/scintilla-base.rb
@@ -1,37 +1,45 @@
 module Scintilla
   class ScintillaBase
     def method_missing(method_name, *args)
-      if method_name.to_s[0..3].upcase == "SCI_"
-        message_id = "Scintilla::SCI_"+method_name.to_s[4..-1].gsub("_", "").upcase
-        send_message(eval(message_id), *args)
+      if method_name.to_s[0..3].upcase == 'SCI_'
+        message_id = 'SCI_' + method_name.to_s[4..-1].gsub('_', '').upcase
+        if Scintilla.const_defined?(message_id)
+          send_message(Scintilla.const_get(message_id), *args)
+        else
+          raise "#{message_id} unknown"
+        end
       else
-        $stderr.puts "method missing #{method_name}"
+        super
       end
     end
-    
+
+    def respond_to_missing?(sym, _include_private)
+      sym.to_s[0..3].upcase == 'SCI_'
+    end
+
     def sci_set_text(text)
-      self.send_message(SCI_SETTEXT, 0, text)
+      send_message(SCI_SETTEXT, 0, text)
     end
-    
-    def sci_get_focus()
+
+    def sci_get_focus
       ret = send_message(SCI_GETFOCUS)
-      return ( ret != 0 )? true : false
+      ret != 0 ? true : false
     end
-    
+
     def sci_autoc_select(select)
-      self.send_message(SCI_AUTOCSELECT, 0, select)
+      send_message(SCI_AUTOCSELECT, 0, select)
     end
 
-    def sci_get_target_text()
-      self.send_message_get_str(SCI_GETTARGETTEXT)
+    def sci_get_target_text
+      send_message_get_str(SCI_GETTARGETTEXT)
     end
 
-    def sci_autoc_get_current_text()
-      self.send_message_get_str(SCI_AUTOCGETCURRENTTEXT)
+    def sci_autoc_get_current_text
+      send_message_get_str(SCI_AUTOCGETCURRENTTEXT)
     end
 
     def sci_annotation_get_text(line)
-      self.send_message_get_str(SCI_ANNOTATIONGETTEXT, line)
+      send_message_get_str(SCI_ANNOTATIONGETTEXT, line)
     end
   end
 end
--- test/scintilla_base.rb
+++ test/scintilla_base.rb
@@ -1,5 +1,32 @@
-##
-## Scintilla::ScintillaBase Test
-##
+module Scintilla
+  class ScintillaTest < ScintillaBase
+    attr_reader :last_message
 
+    def initialize
+      super
+      @last_message = 0
+    end
 
+    def send_message(message, *_args)
+      @last_message = message
+    end
+  end
+end
+
+assert('Scintilla::ScintillaBase#method_missing') do
+  st = Scintilla::ScintillaTest.new
+  st.sci_set_save_point
+  assert_equal Scintilla::SCI_SETSAVEPOINT, st.last_message
+  st.Sci_ClearAll
+  assert_equal Scintilla::SCI_CLEARALL, st.last_message
+  assert_raise(NoMethodError) { st.hogehoge }
+  assert_raise(RuntimeError) { st.sci_hogehoge }
+  assert_raise(NoMethodError) { st.xxx_sci_hogehoge }
+end
+
+assert('Scintilla::ScintillaBase#respond_to') do
+  st = Scintilla::ScintillaTest.new
+  assert_equal true, st.respond_to?(:sci_get_focus)
+  assert_equal true, st.respond_to?(:sci_set_save_point)
+  assert_equal false, st.respond_to?(:hogehoge)
+end
